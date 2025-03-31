8704e653b6c82f4382bab1d87ee14a7624c14d71
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-03-31 11:26:28 +0530

    make public file accesibble

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(
     credentials: true,
   })
 );
+app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
